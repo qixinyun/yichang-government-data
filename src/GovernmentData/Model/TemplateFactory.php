be203d1f56cb8af95eb20867c51e7f08c49a0724
leonnj1990 <1119679388@qq.com>
2018-07-11 22:25:13 +0800

    update

--- src/GovernmentData/Model/TemplateFactory.php
+++ src/GovernmentData/Model/TemplateFactory.php
@@ -10,7 +10,7 @@ class TemplateFactory
 
     public function getTemplate(string $type) : ITemplate
     {
-        $templateModel = isset(self::MAPS[$type]) ? self::MAPS[$type] : '';
+        $template = isset(self::MAPS[$type]) ? self::MAPS[$type] : '';
 
         return class_exists($template) ? new $template : new NullTemplate();
     }
